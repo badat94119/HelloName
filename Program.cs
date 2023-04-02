368e15b5c54db08d63fc92469d6cf300d97fbe48
badat94119 <datba411@gmail.com>
2023-04-02 18:00:49 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name: ");
+
+            string yourName = Console.ReadLine();
+
+            Console.WriteLine("Hello: " + yourName);
+        }
+    }
+}
